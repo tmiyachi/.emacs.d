01b4f8bb8fb073ce6070cc224cee358d8538672c
Tetsuro Miyachi <t.miyachi@hotmail.co.jp>
2015-12-04 20:30:46 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,154 @@
+;;; ロードパス
+;; ロードパスの追加
+(setq load-path (append
+                 '("~/.emacs.d")
+                 load-path))
+
+
+;;; 日本語環境
+;; Localeに合わせた環境の設定
+(set-locale-environment nil)
+
+
+;;; キーバインド
+;; 基本
+(define-key global-map (kbd "M-?") 'help-for-help)        ; ヘルプ
+(define-key global-map (kbd "C-z") 'undo)                 ; undo
+(define-key global-map (kbd "C-c C-i") 'hippie-expand)    ; 補完
+(define-key global-map (kbd "C-c ;") 'comment-dwim)       ; コメントアウト
+(define-key global-map (kbd "M-C-g") 'grep)               ; grep
+(define-key global-map (kbd "C-[ M-C-g") 'goto-line)      ; 指定行へ移動
+(define-key global-map (kbd "C-[ M-C-a") 'align)          ; 整形
+
+
+;;; grep
+;; 再帰的にgrep
+(require 'grep)
+(setq grep-command-before-query "grep -nH -r -e ")
+(defun grep-default-command ()
+  (if current-prefix-arg
+      (let ((grep-command-before-target
+             (concat grep-command-before-query
+                     (shell-quote-argument (grep-tag-default)))))
+        (cons (if buffer-file-name
+                  (concat grep-command-before-target
+                          " *."
+                          (file-name-extension buffer-file-name))
+                (concat grep-command-before-target " ."))
+              (+ (length grep-command-before-target) 1)))
+    (car grep-command)))
+(setq grep-command (cons (concat grep-command-before-query " .")
+                         (+ (length grep-command-before-query) 1)))
+
+
+;;; バー
+(menu-bar-mode -1)   ; メニューバーを消す
+(tool-bar-mode -1)   ; ツールバーを消す
+
+
+;;; 括弧
+;; 対応する括弧を光らせる。
+(show-paren-mode 1)
+;; ウィンドウ内に収まらないときだけ括弧内も光らせる。
+(setq show-paren-style 'mixed)
+
+
+;;; 空白
+;; 空白や長すぎる行を視覚化する。
+(require 'whitespace)
+(setq whitespace-style '(face              ; faceを使って視覚化する。
+                         trailing          ; 行末
+                         tabs              ; タブ
+                         spaces            ; スペース
+                         empty             ; 先頭/末尾の空行
+                         space-mark        ; 表示のマッピング
+                         tab-mark
+                         ))
+(setq whitespace-display-mappings
+      '((space-mark ?\u3000 [?\u25a1])
+        (tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+;; スペースは全角のみを可視化
+(setq whitespace-space-regexp "\\(\u3000+\\)")
+;; 保存前に自動でクリーンアップ
+(setq whitespace-action '(auto-cleanup))
+;; デフォルトで視覚化を有効にする。
+(global-whitespace-mode 1)
+
+(defvar my/bg-color "#232323")
+(set-face-attribute 'whitespace-trailing nil
+                    :background my/bg-color
+                    :foreground "DeepPink"
+                    :underline t)
+(set-face-attribute 'whitespace-tab nil
+                    :background my/bg-color
+                    :foreground "LightSkyBlue"
+                    :underline t)
+(set-face-attribute 'whitespace-space nil
+                    :background my/bg-color
+                    :foreground "GreenYellow"
+                    :weight 'bold)
+(set-face-attribute 'whitespace-empty nil
+                    :background my/bg-color)
+
+;;; 位置
+;; 現在行を目立たせる
+(global-hl-line-mode)
+;; カーソルの位置が何文字目かを表示する
+(column-number-mode t)
+;; カーソルの位置が何行目かを表示する
+(line-number-mode t)
+;; カーソルの場所を保存する
+(require 'saveplace)
+(setq-default save-place t)
+
+
+;;; 行
+;; 行の先頭でC-kを一回押すだけで行全体を消去する
+(setq kill-whole-line t)
+;; 最終行に必ず一行挿入する
+(setq require-final-newline t)
+;; バッファの最後でnewlineで新規行を追加するのを禁止する
+(setq next-line-add-newlines nil)
+
+
+;;; バックアップ
+;; バックアップファイルを作らない
+(setq backup-inhibited t)
+;; 終了時にオートセーブファイルを消す
+(setq delete-auto-save-files t)
+
+
+
+;;; 履歴
+;; 履歴数を大きくする
+(setq history-length 1000)
+;; ミニバッファの履歴を保存する
+(savehist-mode 1)
+;; 最近開いたファイルを保存する数を増やす
+(setq recentf-max-saved-items 1000)
+
+
+;;; 圧縮
+;; gzファイルも編集できるようにする
+(auto-compression-mode t)
+
+
+;;; diff
+;; ediffを1ウィンドウで実行
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+;; diffのオプション
+(setq diff-switches '("-u" "-p" "-N"))
+
+
+;;;
+;; 先頭行に!#があるファイルを保存すると実行権をつける
+(add-hook 'after-save-hook
+          'executable-make-buffer-file-executable-if-script-p)
+
+
+
+;;; 追加の設定
+;; 標準Elisp（Emacs標準搭載のElisp）の設定
+(load "config/builtins")
+;; 非標準Elisp（Emacs非標準搭載のElisp）の設定
+(load "config/packages")
