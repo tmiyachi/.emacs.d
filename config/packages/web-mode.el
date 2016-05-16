f082018b3fdf4cb064b6295d17a8036727a78cb9
tmiyachi <t.miyachi@hotmail.co.jp>
2016-05-16 20:33:46 +0900

    web-modeの表示色を変更

--- config/packages/web-mode.el
+++ config/packages/web-mode.el
@@ -12,3 +12,27 @@
   (setq indent-tabs-mode t)
   (setq tab-width 2))
 (add-hook 'web-mode-hook 'web-mode-hook)
+
+;;; 色の設定
+(custom-set-faces
+ '(web-mode-doctype-face
+   ((t (:foreground "#82AE46"))))                          ; doctype
+ '(web-mode-html-tag-bracket-face
+   ((t (:foreground "#D3D3D3"))))                          ; 括弧
+ '(web-mode-html-tag-face
+   ((t (:foreground "#E6B422" :weight bold))))             ; 要素名
+ '(web-mode-html-attr-name-face
+   ((t (:foreground "#C97586"))))                          ; 属性名など
+ '(web-mode-html-attr-value-face
+   ((t (:foreground "#82AE46"))))                          ; 属性値
+ '(web-mode-comment-face
+   ((t (:foreground "#D9333F"))))                          ; コメント
+ '(web-mode-server-comment-face
+   ((t (:foreground "#D9333F"))))                          ; コメント
+ '(web-mode-css-rule-face
+   ((t (:foreground "#A0D8EF"))))                          ; cssのタグ
+ '(web-mode-css-pseudo-class-face
+   ((t (:foreground "#FF7F00"))))                          ; css 疑似クラス
+ '(web-mode-css-at-rule-face
+   ((t (:foreground "#FF7F00"))))                          ; cssのタグ
+ )
